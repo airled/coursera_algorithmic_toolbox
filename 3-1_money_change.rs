1b12725d5eac72d86f703d0c4e5c0dbde9cf649b
nilstruct <nilstruct@gmail.com>
2025-06-16 15:08:28 +0600

    Add 3-1

---
+++ 3-1_money_change.rs
@@ -0,0 +1,16 @@
+fn main() {
+	let mut input = String::new();
+	let _ = std::io::stdin().read_line(&mut input);
+	input = input.replace("\n", "");
+	let mut sum = input.parse::<u128>().unwrap();
+
+	let coins = [10, 5, 1];
+
+	let mut count = 0;
+	for coin in coins {
+		let amount = sum / coin;
+		count += amount;
+		sum -= coin * amount;
+	}
+	println!("{}", count);
+}
